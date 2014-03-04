96ae39d938bd1a8039f4fa42d10f28aa99f7d010
Sharif Olorin <sio@tesser.org>
2014-03-04 19:23:46 +1100

    Read EPOC frames

--- emogo.go
+++ emogo.go
@@ -2,6 +2,8 @@ package emogo
 
 import (
 	"errors"
+	"unsafe"
+	"syscall"
 )
 
 // #include <emokit/emokit.h>
@@ -14,6 +16,7 @@ import "C"
 const (
 	EMOKIT_VID int = 0x21a1
 	EMOKIT_PID int = 0x0001
+	EmokitPacketSize = 32
 )
 
 // EmokitContext represents a connection to an EPOC device. 
@@ -29,4 +32,45 @@ func NewEmokitContext() (*EmokitContext, error) {
 		return nil, errors.New("Cannot access device.")
 	}
 	return e, nil
+}
+
+type EmokitFrame struct {
+	raw []byte
+	rendered C.struct_emokit_frame
+}
+
+func NewEmokitFrame() *EmokitFrame {
+	f := new(EmokitFrame)
+	f.raw = make([]byte, EmokitPacketSize)
+	return f
+}
+
+// readData reads data from the EPOC dongle and returns the number of
+// bytes read. 
+func (e *EmokitContext) readData() int {
+	n := C.emokit_read_data(e.eeg)
+	return int(n)
+}
+
+func (e *EmokitContext) getNextFrame() (*EmokitFrame, error) {
+	f := NewEmokitFrame()
+	f.rendered = C.emokit_get_next_frame(e.eeg)
+	if f.rendered.counter == 0 {
+		return nil, errors.New("Could not read raw packet.")
+	}
+	C.emokit_get_raw_frame(e.eeg, (*C.uchar)(unsafe.Pointer(&f.raw[0])))
+	return f, nil
+}
+
+// GetFrame returns the next available EPOC frame. If there is no frame
+// to be read, the error value will be EAGAIN.
+func (e *EmokitContext) GetFrame() (*EmokitFrame, error) {
+	if e.readData() > 0 {
+		f, err := e.getNextFrame()
+		if err != nil {
+			return nil, err
+		}
+		return f, nil
+	}
+	return nil, syscall.EAGAIN
 }
